4ca6314edd5dfd2f63def2de7348b4c8fc8ec8fb
xingchentye <xingchenawa@qq.com>
2025-10-03 21:09:26 +0800

    fix bug.

--- app/src/main/java/com/example/todo/network/ApiService.java
+++ app/src/main/java/com/example/todo/network/ApiService.java
@@ -1,11 +1,14 @@
 package com.example.todo.network;
 
 import com.example.todo.model.dto.UserLoginDTO;
+import com.example.todo.model.dto.UserRegisterDTO;
 import com.example.todo.model.response.ApiResponse;
 import com.example.todo.model.vo.UserLoginVO;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 /**
  * API服务接口
@@ -20,4 +23,28 @@ public interface ApiService {
      */
     @POST("/users/login")
     Call<ApiResponse<UserLoginVO>> login(@Body UserLoginDTO userLoginDTO);
+
+    /**
+     * 用户注册接口
+     * @param userRegisterDTO 注册请求数据
+     * @return 注册响应结果
+     */
+    @POST("/users/register")
+    Call<ApiResponse<Void>> register(@Body UserRegisterDTO userRegisterDTO);
+
+    /**
+     * 校验用户名是否可用
+     * @param username 用户名
+     * @return 是否可用
+     */
+    @GET("/users/check-username")
+    Call<ApiResponse<Boolean>> checkUsername(@Query("username") String username);
+
+    /**
+     * 校验邮箱是否可用
+     * @param email 邮箱
+     * @return 是否可用
+     */
+    @GET("/users/check-email")
+    Call<ApiResponse<Boolean>> checkEmail(@Query("email") String email);
 }
